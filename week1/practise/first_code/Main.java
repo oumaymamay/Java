ed41df6302bd97366be9389468faa56f2b3c6e31
oumaymamay <oumayma.mejri11@gmail.com>
2024-09-23 20:36:06 +0200

    first code

---
+++ week1/practise/first_code/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "oumayma";
+        int age = 34;
+        String hometown = "Mannouba";
+        System.out.println("My Name Is " + name);
+        System.out.printf("I am %s years \n" ,age);
+        System.out.println("My Hometown Is  " + hometown);
+    }
+}
